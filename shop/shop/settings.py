368537844652118099ef36b9f8c50e4d407f99d0
email <mmb2020max@gmail.com>
2022-02-13 16:39:34 +0200

    Add csrf

--- shop/shop/settings.py
+++ shop/shop/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'apps.blog.apps.BlogConfig',
+    'apps.blog.apps.FormtestConfig',
+    # 'apps.blog.apps.BlogConfig',
 ]
 
 MIDDLEWARE = [
