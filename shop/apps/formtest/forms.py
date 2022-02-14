e443f375efee8e1963cfc45ce3599e4748072a07
email <mmb2020max@gmail.com>
2022-02-14 14:30:07 +0200

    Description field Added

--- shop/apps/formtest/forms.py
+++ shop/apps/formtest/forms.py
@@ -1,4 +1,5 @@
 from audioop import avg
+from logging import PlaceHolder
 from django import forms
 
 
@@ -28,19 +29,27 @@ class InputForm2(forms.Form):
     color=forms.ChoiceField(choices=FAVORITE_COLOR)
     colors=forms.MultipleChoiceField(choices=FAVORITE_COLOR)
     
-    
+     
     
          
 class widgetForm1(forms.Form):
-    name=forms.CharField(max_length=10 , required=True,label='Name')
+    name=forms.CharField(max_length=10 , required=True,label='Name',widget=forms.TextInput(attrs={'class':'c1','placeholder':'ENTER You Name'}))
     family=forms.CharField(max_length=15, label="Family")
     password=forms.CharField(max_length="10",label="PASSWORD",widget=forms.PasswordInput)
     Year_Choice=['2018','2019','2020','2021','2022'] #Limited years  Added 
     register_date1=forms.DateField(label="Date of Enrolment", required=False,widget=forms.SelectDateWidget(years=Year_Choice))
     register_date2=forms.DateField(label="Date of Enrolment", required=False,widget=forms.NumberInput(attrs={'type':'date'}))
     #attribute added to show its type of  date.anytime we wanted a property and a tag added to html ,use attrs
-    
-    
+    FAVORITE_COLOR=[
+                    ("1","Red"),
+                    ("2","Green"),
+                    ("3","Blue"),
+                    ("4","Yellow"),                             
+                    ]
+    color=forms.ChoiceField(choices=FAVORITE_COLOR)
+    colors=forms.MultipleChoiceField(choices=FAVORITE_COLOR)
+    description=forms.BooleanField(widget=forms.Textarea)
+    description.widget.attrs.update({'class':'c1'})
     
     
     
